25149e3b29b876fd52db022f902a648270bdd5ce
zhangwei <laketea@163.com>
2014-05-19 20:43:25 +0800

    add

---
+++ angularjs-source/src/app.js
@@ -0,0 +1,11 @@
+/**
+ * app.js
+ * Copyright (c) 2013 Youngfriend Inc.
+ * All rights reserved.
+ */
+ angular.module('app', [])
+.controller('AppCtrl', ['$scope', '$element', function AppCtrl($scope, element) {
+
+   console.log('init');
+
+}]);
